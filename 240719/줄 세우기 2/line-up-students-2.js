10d687d79e72364a0d5b545d323a590feb22286c
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2024-07-19 16:04:35 +0900

    Title: 줄 세우기 2, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240719/줄 세우기 2/line-up-students-2.js
@@ -0,0 +1,39 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+const inputs = []
+
+rl.on('line', line => {
+    inputs.push(line.trim())
+})
+
+rl.on('close' , () => {
+    let n = Number(inputs.shift())
+    
+    const students = inputs.map(input => input.split(' ').map(Number))
+    
+    function sortStudent(a,b) {
+        const [aH,aW] = a
+        const [bH,bW] = b
+
+        if(aH < bH) return -1
+        if(aH > bH) return 1
+        if(aW < bW) return 1
+        if(aW > bW) return -1
+
+        return 0
+    }
+
+    students.sort(sortStudent)
+
+    const indexedStudents = students.map((el,idx) => [...el,idx+1])
+
+    for(let student of indexedStudents) {
+        console.log(student.join(' '))
+    }
+
+})
