566a024d8e844f7bc4e301b5e1b0f087986b9afc
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2024-07-28 12:08:38 +0900

    Title: 그 요일은, Time: 263ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240728/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,35 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+const inputs = []
+
+rl.on('line', line => {
+    inputs.push(line.trim())
+})
+
+rl.on('close' , () => {
+    const [m1,d1,m2,d2] = inputs.shift().split(' ').map(Number)
+    const day = inputs.shift()
+    const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+
+    let cnt = 0
+
+    let currentDay = new Date(2024,m1+1,d1)
+
+    while(currentDay <= new Date(2024,m2+1,d2)) {
+        if(days[currentDay.getDay()] === day) {
+            cnt += 1
+        }
+
+        currentDay.setDate(currentDay.getDate() + 1)
+    }
+
+    console.log(cnt)
+
+
+
+})
